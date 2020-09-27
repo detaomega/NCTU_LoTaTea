287f4807e67ba9ef789ba523ef48c594ee0cdb72
mmi366127 <35695972+mmi366127@users.noreply.github.com>
2020-09-27 14:25:05 +0800

    Create default.cpp

---
+++ codebook/template/default.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define int long long
+#define all(x) begin(x), end(x)
+#define SZ(x) ((int)(x).size())
+#define EB emplace_back
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define de(x) if(x && x == MODE)
+#define MODE 1
+
+using namespace std;
+
+typedef pair<int, int> pii;
+const int INF = 1e18;
+const int maxn = 1e5 + 7;
+
+
+main() {
+    int i,j;
+
+    
+    return 0;
+}
