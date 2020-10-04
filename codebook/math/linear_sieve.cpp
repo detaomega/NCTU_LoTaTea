89b41f6d578bc177d7e3348450c81656b5f66fb7
detaomega <46601927+detaomega@users.noreply.github.com>
2020-10-04 23:00:38 +0800

    Update linear_sieve.cpp

--- codebook/math/linear_sieve.cpp
+++ codebook/math/linear_sieve.cpp
@@ -5,10 +5,10 @@ vector<int> linear_sieve(int Up_bound) {
 	  primes.push_back(p);
 	  last_prime[p] = p;
     }
-    for (int j = 0; primes[j] * p < Up_bound; j++) {
+    for (int j = 0; j < primes.size() && primes[j] * p < Up_bound; j++) {
       last_prime[primes[j] * p] = primes[j];
       if (p % primes[j] == 0) break;
     }
   }
-  return last_prime;
+  return primes;
 }
