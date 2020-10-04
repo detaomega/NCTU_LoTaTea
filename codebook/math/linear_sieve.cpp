902e8cb9042189900261901f6be923806207319b
detaomega <46601927+detaomega@users.noreply.github.com>
2020-10-04 12:02:10 +0800

    Create linear_sieve.cpp

---
+++ codebook/math/linear_sieve.cpp
@@ -0,0 +1,14 @@
+vector<int> linear_sieve(int Up_bound) {
+  vector<int> primes, last_prime(Up_bound, 0);
+  for (int p = 2; p < UPBD; ++p) {
+    if (!last_prime[p]) {
+	  primes.push_back(p);
+	  last_prime[p] = p;
+    }
+    for (int j = 0; primes[j] * p < UPBD; ++j) {
+      last_prime[primes[j] * p] = primes[j];
+      if (p % primes[j] == 0) break;
+    }
+  }
+  return last_prime;
+}
