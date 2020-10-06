2a6f024d4d571c1d283635990c845151542ec166
detaomega <46601927+detaomega@users.noreply.github.com>
2020-10-06 23:54:14 +0800

    Update and rename codebook/misk/random.cpp to codebook/misc/random.cpp

--- codebook/misc/random.cpp
+++ codebook/misc/random.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int32_t main() {
   mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-	shuffle(arr, arr + n, rng);
-	int k = uniform_int_distribution<int>(1, 100)(rng) ;
+  shuffle(arr, arr + n, rng);
+  int k = uniform_int_distribution<int>(1, 100)(rng) ;
 }
