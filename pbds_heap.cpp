a1d4581ab5e7eec563560f2d5e5d2cb65e237f8c
mmi366127 <35695972+mmi366127@users.noreply.github.com>
2020-10-07 00:29:15 +0800

    Delete pbds_heap.cpp

--- pbds_heap.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/extc++.h>
-typedef __gnu_pbds::priority_queue<int> heap_t;
-int main() { heap_t a, b;
-  a.clear(); a.push(1); a.push(3);
-  b.clear(); b.push(2); b.push(4);
-  assert(a.top() == 3); assert(b.top() == 4);
-  a.join(b);
-  assert(a.top() == 4); assert(b.empty());
-}
