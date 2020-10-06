15e8ec42968641990813878a56b1f6c3f8948645
detaomega <46601927+detaomega@users.noreply.github.com>
2020-10-06 23:53:21 +0800

    Create random.cpp

---
+++ codebook/misk/random.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main() {
+  mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	shuffle(arr, arr + n, rng);
+	int k = uniform_int_distribution<int>(1, 100)(rng) ;
+}
