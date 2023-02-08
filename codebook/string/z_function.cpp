09c8945e2c0db795e9a0bf932abb5546882791e9
IceTea233 <47026460+IceTea233@users.noreply.github.com>
2023-02-08 23:52:35 +0800

    Add files via upload

---
+++ codebook/string/z_function.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> build_z(const string &s) {
+    int n = s.size();
+    vector<int> z(n, 0);
+    for (int i = 1, l = 0, r = 0; i < n; i++) {
+        if (i <= r)
+            z[i] = min(r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            z[i]++;
+        if (i + z[i] - 1 > r) {
+            l = i;
+            r = i + z[i] - 1;
+        }
+    }
+    return z;
+}
